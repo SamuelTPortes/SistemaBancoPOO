93162623818c162492b00836bb9953cef3211a3d
Samuel <samueltportes@gmail.com>
2025-04-28 21:41:30 -0300

    Quase terminado

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -3,7 +3,7 @@ public class BankAccount {
     private float specialCheck;
     private final String ownerName;
     private boolean usingSpecialCheck;
-
+    private float accumulatedSpecialCheck;
     public String getOwnerName() {
         return ownerName;
     }
@@ -25,46 +25,73 @@ public class BankAccount {
     public void checkBalance(){
         System.out.printf("Seu saldo atual é de R$ %.2f", getBalance());
     }
+
     public void checkSpecialCheck(){
-        System.out.printf("Seu cheque especial atual é de R$ %.2f", getSpecialCheck());
+        System.out.printf("Seu cheque especial atual é de R$ %.2f\n", getSpecialCheck());
     }
+
     public void deposit(float quantity){
         changeBalance(quantity);
-        System.out.printf("Você depositou um total de R$ %.2f " + " Seu saldo atual é de R$ %.2f",quantity,getBalance());
+        System.out.printf("Você depositou um total de R$ %.2f " + " Seu saldo atual é de R$ %.2f\n",quantity,getBalance());
     }
 
     public void withdraw(float quantity){
-        changeBalance(-quantity);
-        System.out.printf("Você sacou um total de R$ %.2f " + " Seu saldo atual é de R$ %.2f",quantity,getBalance());
-    }
-
-    public void payBill(float value){
-        changeBalance(-value);
-        System.out.printf("Você pagou um boleto no total de R$ %.2f " + " Seu saldo atual é de R$ %.2f",value,getBalance());
-    }
-
-    private float changeBalance(float value){
-        balance += value;
-        return balance;
-    }
-
-    public void usingSpecialCheck(){
-        isUsingSpecialCheck();
-        if(usingSpecialCheck) System.out.println("Você está usando o seu cheque especial");
-    }
-
-    private void isUsingSpecialCheck(){
-        if(getBalance() <= 0 & !usingSpecialCheck){
-            usingSpecialCheck = true;
-            changeBalance(getSpecialCheck());
-        } else usingSpecialCheck = false;
-        while(usingSpecialCheck){
-            specialCheck = balance;
+        if((getBalance() - quantity) <= 0 | usingSpecialCheck) System.out.println("Você não tem saldo o suficiente ou está utilizando o valor do cheque especial");
+        else {
+            changeBalance(-quantity);
+            System.out.printf("Você sacou um total de R$ %.2f " + " Seu saldo atual é de R$ %.2f\n", quantity, getBalance());
         }
     }
 
-//    private void isNotUsingSpecialCheck(){
-//        if()
-//    }
+    public void payBill(float value){
+        if((getBalance() - value) < 0 | usingSpecialCheck){
+            System.out.println("Você não tem saldo o suficiente, porém agora utilizaremos o cheque especial!");
+            if((getBalance() - value) < 0 & usingSpecialCheck){
+                System.out.println("O seu saldo atual, mesmo junto ao cheque especial, não paga o valor do boleto, sinto muito!");
+            } else{
+
+                if(getBalance() - value < 0) debt(value - getBalance());
+                else debt(value);
+
+                changeBalance(-value);
+
+                if(!usingSpecialCheck) isUsingSpecialCheck();
+
+                System.out.printf("Você pagou um boleto no total de R$ %.2f " + " Seu saldo atual é de R$ %.2f\n",value,getBalance());
+            }
+        } else{
+            changeBalance(-value);
+            System.out.printf("Você pagou um boleto no total de R$ %.2f " + " Seu saldo atual é de R$ %.2f\n",value,getBalance());
+            }
+    }
+
+    private void changeBalance(float value){
+        balance += value;
+    }
+
+    public void usingSpecialCheck(){
+        if(usingSpecialCheck) System.out.println("Você está usando o seu cheque especial");
+        else System.out.println("Você não está usando o seu cheque especial");
+    }
+
+    private void isUsingSpecialCheck(){
+        if(getBalance() < 0 & !usingSpecialCheck){
+            usingSpecialCheck = true;
+            changeBalance(getSpecialCheck());
+            specialCheck = balance;
+            System.out.println("AVISO! A PARTIR DE AGORA VOCÊ ESTARÁ USANDO O CHEQUE ESPECIAL, LOGO, ASSIM QUE VOCÊ DEPOSITAR ALGUM DINHEIRO, SERÁ COBRADO 20% DO VALOR QUE VOCÊ UTILIZOU DO CHEQUE ESPECIAL");
+        } else usingSpecialCheck = false;
+    }
+
+    private void debt(float value){
+        accumulatedSpecialCheck += value;
+        System.out.println("valor acumulado" + accumulatedSpecialCheck*0.2f);
+    }
+
+    private void payYourDebt(float value){
+        if(getBalance() + value >= accumulatedSpecialCheck){
+
+        }
+    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        sc.useDelimiter("\n");
         System.out.println("Bem vindo ao processo de criação de conta!\nInforme seu nome e o valor desejado para depósito inicial");
 
         String input = "";
@@ -13,23 +14,58 @@ public class Main {
 
         BankAccount bankAccount = new BankAccount(sc.next(), sc.nextFloat());
 
-        System.out.printf("Olá " + bankAccount.getOwnerName() + " que bom te ver por aqui!\nSeu saldo é de: %.2f" + "\nSeu cheque especial é de: %.2f\n", bankAccount.getBalance(),bankAccount.getSpecialCheck());
+        System.out.printf("Olá " + bankAccount.getOwnerName() + " que bom te ver por aqui!\nSeu saldo é de: R$ %.2f" + "\nSeu cheque especial é de: R$ %.2f\n", bankAccount.getBalance(),bankAccount.getSpecialCheck());
         System.out.println("Você tem a liberdade de consultar o seu saldo e seu cheque especial, depositar, sacar, pagar algum boleto, e verificar se a sua conta está usando cheque especial");
-        System.out.println("Os comandos são os seguintes: consultar saldo | consultar cheque | depositar | sacar | pagar boleto | verificar cheque");
+
 
         while(isInputing) {
+            System.out.println("\nOs comandos são os seguintes:\n| consultar saldo | consultar cheque | depositar |\n| sacar | pagar boleto | verificar cheque |\n| sair |");
             input = sc.next();
             input.toLowerCase();
             isInputing = false;
-        }
-        if(oldInput != input){
-            switch (input){
+        if(input != oldInput) {
+            switch (input) {
                 case "consultar saldo":
                     bankAccount.checkBalance();
                     isInputing = true;
                     oldInput = input;
-                    break; //
+                    break;
+                case "consultar cheque":
+                    bankAccount.checkSpecialCheck();
+                    isInputing = true;
+                    oldInput = input;
+                    break;
+                case "depositar":
+                    System.out.println("Insira o valor a depositar");
+                    float value = sc.nextFloat();
+                    bankAccount.deposit(value);
+                    isInputing = true;
+                    oldInput = input;
+                    break;
+                case "sacar":
+                    System.out.println("Insira o valor a sacar");
+                    float value1 = sc.nextFloat();
+                    bankAccount.withdraw(value1);
+                    isInputing = true;
+                    oldInput = input;
+                    break;
+                case "pagar boleto":
+                    System.out.println("Insira o valor do boleto");
+                    float value2 = sc.nextFloat();
+                    bankAccount.payBill(value2);
+                    isInputing = true;
+                    oldInput = input;
+                    break;
+                case "verificar cheque":
+                    bankAccount.usingSpecialCheck();
+                    isInputing = true;
+                    oldInput = input;
+                    break;
+                case "sair":
+                    break;
             }
+
+        }
         }
     }
 }
